674fe608dc07a0b432ebbb4184437d1e45bf2aad
MacRat <m@crat.jp>
2019-10-01 23:25:37 +0900

    Made initial screen

--- raspi-signage/files/initial-screen/main.py
+++ raspi-signage/files/initial-screen/main.py
@@ -4,11 +4,15 @@ import time
 import typing
 
 from PIL import Image, ImageDraw, ImageFont
+import qrcode
 
 
-BASE_IMAGE = "/home/signage/raspi-signage/assets/logo.png"
+BASE_IMAGE = "/home/signage/initial-screen/initial-screen-base.png"
 FONT_FILE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
-OUTPUT_TO = "/var/tmp/wait-screen.png"
+OUTPUT_TO = "/var/tmp/initial-screen.png"
+HOSTAPD_CONF = "/etc/hostapd/hostapd.conf"
+
+CONSOLE_URL = "http://example.com:8080"
 
 
 def get_addresses() -> typing.List[typing.Tuple[str, str]]:
@@ -29,6 +33,20 @@ def get_addresses() -> typing.List[typing.Tuple[str, str]]:
     )
 
 
+def get_wifi_info() -> typing.Tuple[str, str]:
+    ssid = ""
+    password = ""
+    for line in open(HOSTAPD_CONF):
+        m = re.match(r"^ssid=(?P<ssid>.*)|wpa_passphrase=(?P<password>.*)$", line)
+        if m:
+            if m.group("ssid"):
+                ssid = m.group("ssid")
+            if m.group("password"):
+                password = m.group("password")
+
+    return ssid, password
+
+
 def generate(
     addresses: typing.List[typing.Tuple[str, str]],
     base: str = BASE_IMAGE,
@@ -45,6 +63,30 @@ def generate(
     for i, (interface, address) in enumerate(addresses):
         if_ = " " * (longest_if - len(interface)) + interface
         draw.text((20, i * 40), f"{if_}: {address}", font=font, fill=(64, 64, 64, 255))
+
+    qr = qrcode.QRCode(border=0)
+    ssid, password = get_wifi_info()
+    qr.add_data(f"WIFI:T:WPA;S:{ssid};P:{password};;")
+    qr.make()
+    img.paste(
+        qr.make_image(fill_color="black", back_color="white").resize((230, 230)),
+        (180, 500),
+        qr.make_image(fill_color="white", back_color="black")
+        .resize((230, 230))
+        .convert("L"),
+    )
+    draw.text((170, 740), f"{ssid} / {password}", font=font, fill=(64, 64, 64, 255))
+
+    qr = qrcode.QRCode(border=0)
+    qr.add_data(CONSOLE_URL)
+    qr.make()
+    img.paste(
+        qr.make_image(fill_color="black", back_color="white").resize((230, 230)),
+        (768 - 160, 500),
+        qr.make_image(fill_color="white", back_color="black")
+        .resize((230, 230))
+        .convert("L"),
+    )
 
     img.save(output)
 
